5b7336752cd42e9a613e65a7eee13535b8d2d212
MRohan46 <rohanmuhammad222k@gmail.com>
2025-02-10 18:20:03 +0500

    Updated backend URL and CORS settings

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chatapp-two-cyan.vercel.app"
+export const host = "https://chat-app-production-river-waves.up.railway.app"
 export const registerRoute = `${host}/api/auth/register`
 export const loginRoute = `${host}/api/auth/login`
 export const setAvatarRoute = `${host}/api/auth/setAvatar`
